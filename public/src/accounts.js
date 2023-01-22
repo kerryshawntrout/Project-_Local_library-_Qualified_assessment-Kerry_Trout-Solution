10827d1a439cc89f05ffaf0ad252750eab852d0d
Kerry S. Trout <kstrout155@gmail.com>
2023-01-21 19:18:55 -0500

    adding files made changes to accounts and books

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -8,11 +8,59 @@ function findAccountById(accounts, id) {
   return targetAccount;
 }
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  let sortedAccounts = accounts.sort(function (a, b) {
+    let nameA = a.name.last;
+    let nameB = b.name.last;
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedAccounts;
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  let allBorrows = 0;
+  for (let i = 0; i < books.length; i++) {
+    for (let j = 0; j < books[i].borrows.length; j++) {
+      if (account.id === books[i].borrows[j].id) {
+        allBorrows += 1;
+      }
+    }
+  }
+  return allBorrows;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let possessed = [];
+  let borrowMatch = [];
+  books.forEach((item) => {
+    const borrowed = item.borrows;
+    const book = {
+      id: item.id,
+      title: item.title,
+      genre: item.genre,
+      authorId: item.authorId,
+      author: {},
+      borrows: {},
+    };
+    const { id, title, genre, authorId, author, borrows } = book;
+
+    borrowed.forEach((borrow) => {
+      if (borrow.id === account.id && borrow.returned === false) {
+        possessed.push(book);
+        borrowMatch.push(borrow);
+        book.borrows = borrowMatch;
+        book.author = authors.filter((auth) => auth.id === book.authorId)[0];
+      }
+    });
+  });
+  return possessed;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,31 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  let correctAuthor = authors.find((author) => author.id === id);
+  return correctAuthor;
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  let correctBook = books.find((book) => book.id === id);
+  return correctBook;
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let finalAnswer = [];
+  let booksIn = [];
+  let booksOut = [];
+  for (i = 0; i < books.length; i++) {
+    if (books[i].borrows[0].returned === true) {
+      booksIn.push(books[i]);
+    } else {
+      booksOut.push(books[i]);
+    }
+  }
+  finalAnswer = [[...booksOut], [...booksIn]];
+  return finalAnswer;
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  
+}
 
 module.exports = {
   findAuthorById,
