9d4126564b2f153453c2cbc6bd9a3cfab24f1d3c
Kerry S. Trout <kstrout155@gmail.com>
2023-01-21 20:47:12 -0500

    finished making changes

--- public/src/home.js
+++ public/src/home.js
@@ -15,15 +15,49 @@ function getBooksBorrowedCount(books) {
 }
 
 function getMostCommonGenres(books) {
-
+  let map = {};
+  books.forEach((num) => {
+    if (map[num.genre]) {
+      map[num.genre]++;
+    } else {
+      map[num.genre] = 1;
+    }
+  });
+  return Object.entries(map)
+    .map(([name, count]) => {
+      return {
+        name,
+        count,
+      };
+    })
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
 }
 
 function getMostPopularBooks(books) {
-
+  return books
+    .map((book) => {
+      return { name: book.title, count: book.borrows.length };
+    })
+    .sort((a, b) => (a.count < b.count ? 1 : -1))
+    .slice(0, 5);
 }
 
 function getMostPopularAuthors(books, authors) {
-  
+  let mostPopularAutorList = [];
+  authors.forEach((author) => {
+    let writer = {
+      name: `${author.name.first} ${author.name.last}`,
+      count: 0,
+    };
+    books.forEach((book) => {
+      if (book.authorId === author.id) {
+        writer.count += book.borrows.length;
+      }
+    });
+    mostPopularAutorList.push(writer);
+  });
+  return mostPopularAutorList.sort((a, b) => b.count - a.count).slice(0, 5);
 }
 
 module.exports = {
